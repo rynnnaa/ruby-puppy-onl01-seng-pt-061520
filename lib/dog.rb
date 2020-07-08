4f0640e86881f1fadb66960123a91d0632a37dc5
Ryna Truong <rynatruong@outlook.com>
2020-07-08 03:44:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
-# Add your code here
+class Dog 
+  @@all = []
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.print_all
+    puts @@all.name
+  end
+  
+  def self.save
+    @@all << self
+  end
+
+end
